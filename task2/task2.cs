29903275e82d4a48dd49ca329b91423590c792f6
thyuzz <44563483+thyuzz@users.noreply.github.com>
2022-10-14 15:46:34 +0300

    Update

--- task2/task2.cs
+++ task2/task2.cs
@@ -1,4 +1,4 @@
-﻿/* Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+/* Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
 452 -> 11
 
@@ -8,11 +8,14 @@
 
 Console.Write("введите число: ");
 int number = int.Parse(Console.ReadLine());
-int Sum = 0;
 
-while (number != 0) {
-    Sum = Sum + (number % 10);
-    number = number/10;
+int Summa(int x) {
+    int Sum = 0;
+    while (x != 0) {
+    Sum = Sum + (x % 10);
+    x = x/10;
+    }
+    return Sum;
 }
 
-Console.Write("Сумма цифр числа: " + Sum);
+Console.Write("Сумма цифр числа: " + Summa(number));
